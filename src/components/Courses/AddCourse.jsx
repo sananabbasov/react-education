84cea52e7db2c13eaccc3072abd86062ea590776
sananabbasov <sanan_abbasov@list.ru>
2022-11-01 13:28:06 +0400

    01.11.2022

--- src/components/Courses/AddCourse.jsx
+++ src/components/Courses/AddCourse.jsx
@@ -1,12 +1,42 @@
-import { Box, FormControl, Grid, InputLabel, MenuItem, Select, TextField } from '@mui/material'
-import React, { useState } from 'react'
+import { Accordion, AccordionDetails, AccordionSummary, Box, Button, FormControl, Grid, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
 
 function AddCourse() {
     const [category, setCategory] = useState('');
+    const [contentName, setContentName] = useState('');
+    const [lessonName, setLessonName] = useState('');
+    const [videoUrl, setVideoUrl] = useState('');
+    const [courseContents, setCourseContents] = useState([])
+    const [contentsCount, setContentsCount] = useState([1]);
+    const [check, setCheck] = useState(false)
 
     const handleChange = (event) => {
         setCategory(event.target.value);
     };
+
+
+    const addContentHandeler = () => {
+        let content = contentsCount;
+        content.push(1)
+        setContentsCount(content)
+        setCheck(!check)
+    }
+
+    const createLessons = (index) => {
+        let data = [
+            {
+                "lessonName": `${lessonName}`,
+                "videoUrl": `${videoUrl}`
+            }
+        ]
+        setCourseContents([...courseContents,...data])
+    }
+
+    console.log(courseContents)
+
+
+    useEffect(() => {
+    }, [check])
     return (
         <div>
             <Grid container width="60%" margin="auto">
@@ -40,8 +70,52 @@ function AddCourse() {
 
                         </Select>
                     </FormControl>
+
+                    <Button variant="contained" onClick={() => addContentHandeler()}>Add chapter</Button>
+                    {
+                        contentsCount.map((x, index) => (
+                            <Accordion key={index} marginTop="50px">
+                                <AccordionSummary
+                                    expandIcon={""}
+                                    aria-controls="panel1a-content"
+                                    id="panel1a-header"
+                                >
+                                    <Typography>Course content {index + 1}</Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <Typography>
+                                        <Grid item lg="12" margin="auto" marginTop="10px">
+                                            <TextField id="outlined-basic" onChange={(e) => setContentName(e.target.value)} label="Chapter name" variant="outlined" fullWidth />
+                                        </Grid>
+                                        <Button variant="contained" onClick={() => createLessons(index + 1)}>Add lesson</Button>
+                                        <Grid container>
+                                            <Grid item lg="6" margin="auto" marginTop="10px">
+                                                <TextField id="outlined-basic" onChange={(e) => setLessonName(e.target.value)} label="Lesson name" variant="outlined" fullWidth />
+                                            </Grid>
+                                            <Grid item lg="6" margin="auto" marginTop="10px">
+                                                <TextField id="outlined-basic" onChange={(e) => setVideoUrl(e.target.value)} label="Lesson url" variant="outlined" fullWidth />
+                                            </Grid>
+                                        </Grid>
+                                    </Typography>
+                                </AccordionDetails>
+                            </Accordion>
+                        ))
+                    }
                 </Box>
+
+
             </Grid>
+
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
 
         </div>
     )
