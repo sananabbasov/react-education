748160f8717c0aef9e9a03663f2ecc3b45c0960e
sananabbasov <sanan_abbasov@list.ru>
2022-11-03 16:14:08 +0400

    03.11.2022

--- src/components/Courses/AddCourse.jsx
+++ src/components/Courses/AddCourse.jsx
@@ -1,5 +1,6 @@
 import { Accordion, AccordionDetails, AccordionSummary, Box, Button, FormControl, Grid, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
+import { BASE_URL } from '../../config/api';
 
 function AddCourse() {
     const [category, setCategory] = useState('');
@@ -9,30 +10,76 @@ function AddCourse() {
     const [courseContents, setCourseContents] = useState([])
     const [contentsCount, setContentsCount] = useState([1]);
     const [check, setCheck] = useState(false)
+    const [courses, setCourses] = useState([]);
+    const [name, setName] = useState([]);
+    const [description, setDescription] = useState([]);
+    const [photoUrl, setPhotoUrl] = useState([]);
+    const [price, setPrice] = useState([]);
+    const [categoryId, setCategoryId] = useState([]);
+
 
     const handleChange = (event) => {
         setCategory(event.target.value);
     };
 
+    console.log(category.toString());
+
+
+    const AddData = () => {
+        fetch(`${BASE_URL}/Course/addcourse`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                courseContents: courses,
+                name: name,
+                description: description,
+                photoUrl: photoUrl,
+                userId: 1,
+                raiting: 0,
+                duration: 0,
+                price: price,
+                categoryId: category.toString()
+            })
+        })
+    }
 
     const addContentHandeler = () => {
+        let result = [
+            {
+                "id": contentsCount.length,
+                "contentName": contentName,
+                "contentLessons": courseContents
+
+            }
+        ]
+        setCourses([...courses, ...result])
         let content = contentsCount;
         content.push(1)
         setContentsCount(content)
+        setCourseContents([])
         setCheck(!check)
     }
 
     const createLessons = (index) => {
         let data = [
             {
+                "id": index,
                 "lessonName": `${lessonName}`,
                 "videoUrl": `${videoUrl}`
             }
         ]
-        setCourseContents([...courseContents,...data])
+        setCourseContents([...courseContents, ...data])
+
+
+
     }
 
+
+
     console.log(courseContents)
+    console.log(courses)
 
 
     useEffect(() => {
@@ -41,16 +88,16 @@ function AddCourse() {
         <div>
             <Grid container width="60%" margin="auto">
                 <Grid item lg="8" margin="auto" marginTop="50px">
-                    <TextField id="outlined-basic" label="Course name" variant="outlined" fullWidth />
+                    <TextField id="outlined-basic" onChange={(e) => setName(e.target.value)} label="Course name" variant="outlined" fullWidth />
                 </Grid>
                 <Grid item lg="8" margin="auto" marginTop="50px">
-                    <TextField id="outlined-basic" label="Course description" variant="outlined" fullWidth />
+                    <TextField id="outlined-basic" onChange={(e) => setDescription(e.target.value)} label="Course description" variant="outlined" fullWidth />
                 </Grid>
                 <Grid item lg="8" margin="auto" marginTop="50px">
-                    <TextField id="outlined-basic" label="Course photo url" variant="outlined" fullWidth />
+                    <TextField id="outlined-basic" onChange={(e) => setPhotoUrl(e.target.value)} label="Course photo url" variant="outlined" fullWidth />
                 </Grid>
                 <Grid item lg="8" margin="auto" marginTop="50px">
-                    <TextField id="outlined-basic" label="Course price" variant="outlined" fullWidth />
+                    <TextField id="outlined-basic" onChange={(e) => setPrice(e.target.value)} label="Course price" variant="outlined" fullWidth />
                 </Grid>
 
                 <Box container width="67%" margin="auto" marginTop="50px">
@@ -64,9 +111,9 @@ function AddCourse() {
                             onChange={handleChange}
                         >
                             <MenuItem value={1}>C#</MenuItem>
+                            <MenuItem value={4}>MERN</MenuItem>
                             <MenuItem value={2}>Java</MenuItem>
                             <MenuItem value={3}>Python</MenuItem>
-                            <MenuItem value={4}>MERN</MenuItem>
 
                         </Select>
                     </FormControl>
@@ -97,17 +144,36 @@ function AddCourse() {
                                             </Grid>
                                         </Grid>
                                     </Typography>
+                                    <Typography>
+                                        {
+
+                                            courses.filter(x => x.id == index + 1).map((data, index) => (
+                                                <div key={index} className="div">
+                                                    {
+                                                        data.contentLessons.map((x,id)=>(
+                                                            <div key={id}>
+                                                                {x.lessonName}
+                                                            </div>
+                                                        ))
+                                                    }
+                                                </div>
+                                            ))
+                                        }
+                                    </Typography>
                                 </AccordionDetails>
                             </Accordion>
                         ))
                     }
                 </Box>
 
+                
 
             </Grid>
-
+            <Grid container width="40%" margin="auto">
             <br />
-            <br />
+                <br />
+                <Button variant="contained" onClick={() => AddData()}>Submit</Button>
+            </Grid>
             <br />
             <br />
             <br />
